1cba8c8668974261dcada39a00878143b0eca425
Maysummer <udojyk@gmail.com>
2023-12-07 21:55:31 +0100

    animate picture

--- src/pages/styles/aboutStyles.ts
+++ src/pages/styles/aboutStyles.ts
@@ -17,9 +17,50 @@ export const AboutContent = styled.div`
   align-items: center;
   gap: 100px;
   font-size: larger;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 50px;
+  }
 `;
 
 export const AboutImage = styled.img`
   height: 20em;
   border-radius: 1em;
+  box-shadow: 5px 8px rgba(24, 31, 25, 0.8);
+
+  &:hover {
+    animation-name: img-animation;
+    animation-duration: 2.5s;
+    animation-iteration-count: infinite;
+    transform-origin: 70% 70%;
+    display: inline-block;
+
+    @keyframes img-animation {
+      0% {
+        transform: rotate(0deg);
+      }
+      10% {
+        transform: rotate(15deg);
+      }
+      20% {
+        transform: rotate(-10deg);
+      }
+      30% {
+        transform: rotate(14deg);
+      }
+      40% {
+        transform: rotate(-4deg);
+      }
+      50% {
+        transform: rotate(10deg);
+      }
+      60% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(0deg);
+      }
+    }
+  }
 `;
