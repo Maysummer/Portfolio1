872171a2f5d0890edba6176dadc933bbbfde3424
Maysummer <udojyk@gmail.com>
2024-05-30 20:50:38 +0100

    implement email js

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,4 @@
-import {
-  FormContainer,
-  Input,
-  Submit,
-  TextArea,
-} from "./styles/contactStyles";
+import { FormContainer, Input, Submit, TextArea } from "./styles/contactStyles";
 import {
   SectionContainer,
   SectionHeader,
@@ -12,24 +7,47 @@ import {
 } from "./styles/sectionStyles";
 import "../index.css";
 import emailjs from "@emailjs/browser";
+import React, { useRef } from "react";
 
 export default function Contact() {
+  const formRef = useRef<HTMLFormElement | null>(null);
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    {
+      formRef.current &&
+        emailjs
+          .sendForm("service_admklbh", "template_c3q715l", formRef.current, {
+            publicKey: "1jcND90J3xuN_bhIP",
+          })
+          .then(
+            () => {
+              console.log("SUCCESS!");
+            },
+            (error) => {
+              console.log("FAILED...", error.text);
+            }
+          );
+    }
+  }
+
   return (
     <SectionContainer id="contact">
       <SectionHeaderContainer>
         <SectionHeaderText>Where to find me</SectionHeaderText>
         <SectionHeader>Contact</SectionHeader>
       </SectionHeaderContainer>
-      <form>
+      <form onSubmit={handleSubmit} ref={formRef}>
         <FormContainer>
-          <Input type="text" placeholder="Name" />
-          <Input type="email" placeholder="Email address" />
+          <Input type="text" placeholder="Name" name="name" />
+          <Input type="email" placeholder="Email address" name="email" />
           <TextArea
-            name=""
             id=""
             cols={30}
             rows={10}
             placeholder="Message"
+            name="message"
           ></TextArea>
           <Submit>Send</Submit>
         </FormContainer>
